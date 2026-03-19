c3317ac9a577e7120cd50e54ee518302f58566b4
Pavel Korolev <p.koror56@gmail.com>
2026-03-19 13:15:37 +0500

    Cleanup: remove unused .vscode settings, delete empty Habr parser file, and simplify conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,6 @@
 import httpx
-import pytest
 
 
 class DummyHttpClient:
     async def get(self, *args: object, **kwargs: object) -> httpx.Response:
         raise NotImplementedError
-
-
-@pytest.fixture()
-async def dummy_http_client() -> DummyHttpClient:
-    return DummyHttpClient()
